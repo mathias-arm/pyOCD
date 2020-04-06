4ca98264cde4d692a85f8166d71bcaf33b7d3727
Chris Reed <flit@me.com>
2020-04-06 17:07:54 -0500

    SDC-600: completed API.

--- pyocd/coresight/sdc600.py
+++ pyocd/coresight/sdc600.py
@@ -16,6 +16,7 @@
 
 import logging
 from time import sleep
+from enum import Enum
 
 from .component import CoreSightComponent
 from ..core import exceptions
@@ -24,12 +25,23 @@ from ..utility.hex import dump_hex_data
 
 LOG = logging.getLogger(__name__)
 
+class UnexpectedFlagError(exceptions.Error):
+    """! @brief Received an unexpected or out of order flag byte."""
+    pass
+
 class SDC600(CoreSightComponent):
     """! @brief SDC-600 component.
     """
     
     ## Timeout for each byte transfer.
-    TRANSFER_TIMEOUT = 10.0
+    TRANSFER_TIMEOUT = 30.0
+    
+    class LinkPhase(Enum):
+        """! @brief COM Port link phases."""
+        ## Hardware-defined link phase.
+        PHASE1 = 1
+        ## Software-defiend link phase.
+        PHASE2 = 2
     
     class Register:
         """! @brief Namespace for register offset constants."""
@@ -86,10 +98,31 @@ class SDC600(CoreSightComponent):
         LPH2RA  = 0xA8
         LPH2RL  = 0xA9
         LPH2RR  = 0xAA
+        LERR    = 0xAB
         START   = 0xAC
         END     = 0xAD
         ESC     = 0xAE
         NULL    = 0xAF
+        
+        # All bytes with 0b101 in bits [7:5] are flag bytes.
+        MASK = 0xE0
+        IDENTIFIER = 0b10100000
+        
+        ## Map from flag value to name.
+        NAME = {
+            IDR     : "IDR",
+            IDA     : "IDA",
+            LPH1RA  : "LPH1RA",
+            LPH1RL  : "LPH1RL",
+            LPH2RA  : "LPH2RA",
+            LPH2RL  : "LPH2RL",
+            LPH2RR  : "LPH2RR",
+            LERR    : "LERR",
+            START   : "START",
+            END     : "END",
+            ESC     : "ESC",
+            NULL    : "NULL",
+            }
     
     ## NULL bytes must be written to the upper bytes, and will be present in the upper bytes
     # when read.
@@ -101,7 +134,10 @@ class SDC600(CoreSightComponent):
         self._rx_width = 0
 
     def init(self):
-        """! @brief Inits the component."""
+        """! @brief Inits the component.
+        
+        Reads the RX and TX widths and whether the SDC-600 is enabled. All error flags are cleared.
+        """
         fidtx = self.ap.read32(self.Register.FIDTXR)
         LOG.info("fidtx=0x%08x", fidtx)
         fidrx = self.ap.read32(self.Register.FIDRXR)
@@ -124,9 +160,12 @@ class SDC600(CoreSightComponent):
     def is_reboot_request_enabled(self):
         return (self.ap.read32(self.Register.SR) & self.Register.SR_RRDIS_MASK) == 0
 
-    def _read(self, count=1):
-        data = []
-        while count:
+    def _read1(self):
+        """! @brief Read a single byte.
+        
+        If a NULL byte is received, it is ignored and another byte is read.
+        """
+        while True:
             # Wait until a byte is ready in the receive FIFO.
             with Timeout(self.TRANSFER_TIMEOUT) as to_:
                 while to_.check():
@@ -138,52 +177,161 @@ class SDC600(CoreSightComponent):
 
             # Read the data register and strip off NULL bytes in high bytes.
             value = self.ap.read32(self.Register.DR) & 0xFF
-            data.append(value)
-            count -= 1
-        return data
+
+            # Ignore NULL flag bytes.
+            if value == self.Flag.NULL:
+                continue
+            
+            return value
         
-    def _write(self, data):
-        for value in data:
-            # Wait until room is available in the transmit FIFO.
-            with Timeout(self.TRANSFER_TIMEOUT) as to_:
-                while to_.check():
-                    if (self.ap.read32(self.Register.SR) & self.Register.SR_TXS_MASK) != 0:
-                        break
-                    sleep(0.01)
-                else:
-                    raise exceptions.TimeoutError("timeout while writing to from SDC-600")
+    def _write1(self, value):
+        """! @brief Write one or more bytes."""
+        # Wait until room is available in the transmit FIFO.
+        with Timeout(self.TRANSFER_TIMEOUT) as to_:
+            while to_.check():
+                if (self.ap.read32(self.Register.SR) & self.Register.SR_TXS_MASK) != 0:
+                    break
+                sleep(0.01)
+            else:
+                raise exceptions.TimeoutError("timeout while writing to from SDC-600")
 
-            # Write this byte to the transmit FIFO.
-            dbr_value = self.NULL_FILL | (value & 0xFF)
-            self.ap.write32(self.Register.DR, dbr_value)
-    
-    def connect(self):
-        LOG.info("sending LPH1RA")
-        self._write([self.Flag.LPH1RA])
-        data = self._read()
-        LOG.info("received:")
-        dump_hex_data(data)
-        if data != [self.Flag.LPH1RA]:
-            LOG.error("got %s instead of LPH1RA in response", str(data))
-            return
+        # Write this byte to the transmit FIFO.
+        dbr_value = self.NULL_FILL | (value & 0xFF)
+        self.ap.write32(self.Register.DR, dbr_value)
+
+    def _expect_flag(self, flag):
+        value = self._read1()
+        LOG.info("received: 0x%02x", value)
+        if value != flag:
+            raise UnexpectedFlagError("got {:#04x} instead of expected {} ({:#04x})".format(
+                value, self.Flag.NAME[flag], flag))
         else:
-            LOG.info("got expected LPH1RA")
+            LOG.info("got expected %s", self.Flag.NAME[value])
 
+    def _stuff(self, data):
+        """! @brief Perform COM Encapsulation byte stuffing."""
+        result = []
+        for value in data:
+            # Values matching flag bytes just get copied to output.
+            if (value & self.Flag.MASK) == self.Flag.IDENTIFIER:
+                # Insert escape flag.
+                result.append(self.Flag.ESC)
+                
+                # Invert high bit.
+                value ^= 0x80
+            
+            result.append(value)
+        return result
+
+    def _destuff(self, data):
+        """! @brief Remove COM Encapsulation byte stuffing."""
+        result = []
+        i = 0
+        while i < len(data):
+            value = data[i]
+            
+            # Check for escaped bytes.
+            if value == self.Flag.ESC:
+                # Skip over escape.
+                i += 1
+                
+                # Get escaped byte and invert high bit to destuff it.
+                value = data[i] ^ 0x80
+            
+            result.append(value)
+            
+            i += 1
+        return result
+
+    def _read_packet_data_to_end(self):
+        """! @brief Read an escaped packet from the first message byte to the end.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        result = []
+        while True:
+            value = self._read1()
+            
+            # Check for the packet end marker flag.
+            if value == self.Flag.END:
+                break
+            elif value == self.Flag.LPH2RL:
+                # Target killed the connection in the middle of a packet. At least reply.
+                self._write1(self.Flag.LPH2RL)
+                return []
+            elif value == self.Flag.LERR:
+                # Received a link error flag.
+                return []
+            else:
+                result.append(value)
+            
+        return self._destuff(result)
+
+    def receive_packet(self):
+        """! @brief Read an escaped packet.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        self._expect_flag(self.Flag.START)
+        return self._read_packet_data_to_end()
+    
+    def send_packet(self, data):
+        """! @brief Send an escaped packet.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        self._write1(self.Flag.START)
+        for value in self._stuff(data):
+            self._write1(value)
+        self._write1(self.Flag.END)
+    
+    def open_link(self, phase):
+        """! @brief Send the LPH1RA or LPH2RA flag.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        if phase == self.LinkPhase.PHASE1:
+            # Close link phase 1 first, to put it in a known state.
+            self.close_link(self.LinkPhase.PHASE1)
+        
+            LOG.info("sending LPH1RA")
+            self._write1(self.Flag.LPH1RA)
+            self._expect_flag(self.Flag.LPH1RA)
+        elif phase == self.LinkPhase.PHASE2:
+            LOG.info("sending LPH2RA")
+            self._write1(self.Flag.LPH2RA)
+            self._expect_flag(self.Flag.LPH2RA)
+
+    def close_link(self, phase):
+        """! @brief Send the LPH1RL or LPH2RL flag.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        if phase == self.LinkPhase.PHASE1:
+            LOG.info("sending LPH1RL")
+            self._write1(self.Flag.LPH1RL)
+            self._expect_flag(self.Flag.LPH1RL)
+        elif phase == self.LinkPhase.PHASE2:
+            LOG.info("sending LPH2RL")
+            self._write1(self.Flag.LPH2RL)
+            self._expect_flag(self.Flag.LPH2RL)
+
+    def _log_status(self):
         status = self.ap.read32(self.Register.SR)
         LOG.info("status=0x%08x", status)
-        
-        self.session.target.cores[1].reset()
 
-        LOG.info("sending LPH2RA")
-        self._write([self.Flag.LPH2RA])
-        data = self._read()
-        LOG.info("received:")
-        dump_hex_data(data)
-        if data != [self.Flag.LPH2RA]:
-            LOG.error("got %s instead of LPH2RA in response", str(data))
-            return
-        else:
-            LOG.info("got expected LPH2RA")
+    def read_protocol_id(self):
+        """! @brief Read and return the 6-byte protocol ID.
+        @exception UnexpectedFlagError
+        @exception TimeoutError
+        """
+        self._write1(self.Flag.IDR)
+        self._expect_flag(self.Flag.IDA)
+        return self._read_packet_data_to_end()
+    
+    def send_reboot_request(self):
+        """! @brief Send remote reboot request."""
+        self._write1(self.Flag.LPH2RR)
     
     def __repr__(self):
         return "<SDC-600@{:x}: en={} txw={} rxw={}>".format(id(self),
